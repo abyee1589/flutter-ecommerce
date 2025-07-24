fd2091b4acebd3a8c215db3d9db3563468a88041
afthegreat <abelferede916@gmail.com>
2025-07-24 16:54:35 +0300

    working on Authenticartion repository

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/features/authentication/screens/onboarding.dart';
 import 'package:flutter_app/navigation_menu.dart';
 import 'package:flutter_app/utils/device/scroll_behavior.dart';
 import 'package:flutter_app/utils/theme/theme.dart';
@@ -14,7 +15,7 @@ class App extends StatelessWidget {
         themeMode: ThemeMode.system,
         theme: AbAppTheme.lightTheme,
         darkTheme: AbAppTheme.darkTheme,
-        home: const NavigationMenu(),
+        home: OnBoardingScreen(),
       );
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/features/shop/controllers/home_controller/home_controller.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
-   Get.put(HomeController());
-   runApp(const App());
+Future<void> main() async{
+  Get.put(HomeController());
+  
+  /// Widgets Binding
+  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  
+  /// GetX Local Storage
+  await GetStorage.init();
+
+  /// await splash untill other items load
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  /// Initialize Firebase and Authentication History
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
+  runApp(const App());
 }
